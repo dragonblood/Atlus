903fde5ac9dbb8cbcea912f96db5b0ce14508ec0
vipul petkar <petkar.vipul@gmail.com>
2019-12-17 14:34:24 +0530

    Database Changed from sqlite3 to PostgreSQL

--- main/settings.py
+++ main/settings.py
@@ -83,10 +83,21 @@ DEFAULT_FROM_EMAIL = 'test@example.com'
 # Database
 # https://docs.djangoproject.com/en/2.2/ref/settings/#databases
 
+# DATABASES = {
+#     'default': {
+#         'ENGINE': 'django.db.backends.sqlite3',
+#         'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+#     }
+# }
+
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        'ENGINE': 'django.db.backends.postgresql_psycopg2',
+        'NAME': 'librus',
+        'USER': 'vipul',
+        'PASSWORD': '9923',
+        'HOST': 'localhost',
+        'PORT': '',
     }
 }
 
