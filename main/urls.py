77ca5b2c0ec795e37d227fd26ec8855a76a91ea5
vipul petkar <petkar.vipul@gmail.com>
2020-03-23 18:34:02 +0530

    last

--- main/urls.py
+++ main/urls.py
@@ -12,7 +12,6 @@ urlpatterns = [
     
     path('accounts/', include('accounts.urls')),
     path('atlus/', include('Atlus.urls')),
-    #path('boomerang/', include('Boomerang.urls')),
 
     path('', IndexPageView.as_view(), name='index'),
 
