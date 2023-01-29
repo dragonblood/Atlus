8579ebb9893fb7e8f90efbb53aa1fede15dc31c1
vipul <petkar.vipul@gmail.com>
2023-01-30 01:18:22 +0530

    python version upgraded

--- Atlus/settings.py
+++ Atlus/settings.py
@@ -1,6 +1,6 @@
 import os
 import warnings
-from django.utils.translation import ugettext_lazy as _
+# from django.utils.translation import ugettext_lazy as _
 
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
